f0447533a1e69abecbe28126912399c13a01dd4a
gehrkeb <gehrkeb@uwec.edu>
2014-09-27 03:10:11 -0500

    First Submit

---
+++ app/src/main/java/target/shopping/clerque/MainActivity.java
@@ -0,0 +1,23 @@
+package target.shopping.clerque;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+
+import java.util.ArrayList;
+
+
+public class MainActivity extends Activity {
+    private Button scan;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        scan = (Button)findViewById(R.id.scan);
+//test
+        ArrayList<String> items = new ArrayList<String>();
+        items.add(0,"Dr. Pepper");
+        items.add(1,"Mountain Dew");
+    }
+}
